87ad15934c0d6bc38c63b41403b2909c603b31af
IMAMURA Shohei <s3imamura@gmail.com>
2023-08-13 14:11:04 +0900

    add method

--- manifest.config.ts
+++ manifest.config.ts
@@ -5,7 +5,18 @@ export const manifest = defineManifest({
   name: "chrome-extension-sample",
   description: "sample",
   version: "0.0.1",
+  permissions: ["tabs"],
   action: {
     default_popup: "index.html",
   },
+  background: {
+    service_worker: "src/background",
+    type: "module",
+  },
+  content_scripts: [
+    {
+      matches: ["<all_urls>"],
+      js: ["src/scripts/addOutline"],
+    },
+  ],
 });
